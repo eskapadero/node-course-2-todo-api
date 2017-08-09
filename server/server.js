bd04c141816fed89100667fe63199c39cd349116
eskapadero <eskapadero@gmail.com>
2017-08-09 20:02:57 +0800

    Add return in generateAuthToken

--- server/server.js
+++ server/server.js
@@ -121,16 +121,9 @@ app.post('/users', (req,res) =>{
    var body  = _.pick(req.body, ['email', 'password']); 
    var user = new User(body);
     
-   
-    
-    
-    
    user.save().then(() =>{
-       
-       user.generateAuthToken();
-       
-       
-   }).then((token)=>{
+       return user.generateAuthToken();
+  }).then((token)=>{
        res.header('x-auth', token).send(user);
        
    }).catch((e)=>{
